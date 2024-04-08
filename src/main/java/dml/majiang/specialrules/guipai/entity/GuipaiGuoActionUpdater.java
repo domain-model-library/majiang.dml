8fed18a54cafe7e9f6380a68c5940616986aa77e
neo <13758242402@163.com>
2024-04-08 21:44:21 +0800

    完善鬼牌业务

---
+++ src/main/java/dml/majiang/specialrules/guipai/entity/GuipaiGuoActionUpdater.java
@@ -0,0 +1,57 @@
+package dml.majiang.specialrules.guipai.entity;
+
+import dml.majiang.core.entity.Pan;
+import dml.majiang.core.entity.PanPlayer;
+import dml.majiang.core.entity.PanSpecialRulesState;
+import dml.majiang.core.entity.action.PanPlayerAction;
+import dml.majiang.core.entity.action.da.DaAction;
+import dml.majiang.core.entity.action.guo.GuoAction;
+import dml.majiang.core.entity.action.guo.GuoActionUpdater;
+import dml.majiang.core.entity.action.mo.LundaoMopai;
+import dml.majiang.core.entity.action.mo.MoAction;
+import dml.majiang.core.entity.action.peng.PengAction;
+
+public class GuipaiGuoActionUpdater implements GuoActionUpdater {
+
+    private long panId;
+
+    @Override
+    public void setPanId(long panId) {
+        this.panId = panId;
+    }
+
+    @Override
+    public long getPanId() {
+        return panId;
+    }
+
+    @Override
+    public void updateActions(GuoAction guoAction, Pan pan, PanSpecialRulesState panSpecialRulesState) {
+        pan.playerClearActionCandidates(guoAction.getActionPlayerId());
+        PanPlayer player = pan.findPlayerById(guoAction.getActionPlayerId());
+
+        // 首先看一下,我过的是什么? 是我摸牌之后的胡,杠? 还是我碰之后的杠? 还是别人打出牌之后我可以吃碰杠胡
+        PanPlayerAction causedByAction = guoAction.getCausedByAction();
+        if (causedByAction instanceof MoAction) {// 过的是我摸牌之后的胡,杠
+            // 那要我打牌
+            player.generateDaActions();
+            GuipaiState guipaiState = panSpecialRulesState.findSpecialRuleState(GuipaiState.class);
+            player.removeDaActionCandidate(guipaiState.getGuipaiType());
+        } else if (causedByAction instanceof DaAction) {// 过的是别人打出牌之后我可以吃碰杠胡
+            if (pan.allPlayerHasNoActionCandidates()) {// 如果所有玩家啥也干不了
+                // 打牌那家的下家摸牌 TODO 还没处理牌摸完
+                // 打牌那家的下家摸牌
+                PanPlayer xiajiaPlayer = pan
+                        .findNextMenFengPlayer(pan.findPlayerById(causedByAction.getActionPlayerId()));
+                xiajiaPlayer.addActionCandidate(new MoAction(xiajiaPlayer.getId(), new LundaoMopai()));
+            }
+        } else if (causedByAction instanceof PengAction) {// 过的是我碰之后的杠
+            // 那要我打牌
+            player.generateDaActions();
+            GuipaiState guipaiState = panSpecialRulesState.findSpecialRuleState(GuipaiState.class);
+            player.removeDaActionCandidate(guipaiState.getGuipaiType());
+        } else {
+        }
+    }
+
+}
---
+++ src/main/java/dml/majiang/specialrules/guipai/entity/GuipaiPengActionUpdater.java
@@ -0,0 +1,45 @@
+package dml.majiang.specialrules.guipai.entity;
+
+import dml.majiang.core.entity.Pan;
+import dml.majiang.core.entity.PanPlayer;
+import dml.majiang.core.entity.PanSpecialRulesState;
+import dml.majiang.core.entity.action.peng.PengAction;
+import dml.majiang.core.entity.action.peng.PengActionUpdater;
+
+public class GuipaiPengActionUpdater implements PengActionUpdater {
+
+    private long panId;
+
+    @Override
+    public void setPanId(long panId) {
+        this.panId = panId;
+    }
+
+    @Override
+    public long getPanId() {
+        return panId;
+    }
+
+    @Override
+    public void updateActions(PengAction pengAction, Pan pan, PanSpecialRulesState panSpecialRulesState) {
+        if (pengAction.isBlockedByHigherPriorityAction()) {
+            return;
+        }
+        pan.clearAllPlayersActionCandidates();
+
+        // 刻子杠手牌
+        PanPlayer player = pan.findPlayerById(pengAction.getActionPlayerId());
+        player.tryKezigangshoupaiAndGenerateCandidateAction();
+
+        // 需要有“过”
+        player.checkAndGenerateGuoCandidateAction(pengAction);
+
+        //打牌
+        if (player.getActionCandidates().isEmpty()) {
+            player.generateDaActions();
+            GuipaiState guipaiState = panSpecialRulesState.findSpecialRuleState(GuipaiState.class);
+            player.removeDaActionCandidate(guipaiState.getGuipaiType());
+        }
+
+    }
+}
