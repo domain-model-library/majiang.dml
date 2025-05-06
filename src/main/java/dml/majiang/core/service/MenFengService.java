b61c21d0f8aa2c5cb01b0ea255c2fea7425b02e2
zhengchengdong <zhengchengdong@gmail.com>
2025-05-06 13:58:31 +0800

    不足4人时自定义采用的门风列表

--- src/main/java/dml/majiang/core/service/MenFengService.java
+++ src/main/java/dml/majiang/core/service/MenFengService.java
@@ -10,28 +10,28 @@ import java.util.Random;
 
 public class MenFengService {
     public static void setPlayerMenFengRandomly(long panId, MenFengServiceRepositorySet menFengServiceRepositorySet) {
+        setPlayerMenFengRandomly(panId, new MenFeng[]{MenFeng.dong, MenFeng.nan, MenFeng.xi, MenFeng.bei}, menFengServiceRepositorySet);
+    }
+
+    /**
+     * 随机设置玩家的门风
+     *
+     * @param panId
+     * @param menFengs                    不足4人时需自定义采用的门风列表
+     * @param menFengServiceRepositorySet
+     */
+    public static void setPlayerMenFengRandomly(long panId, MenFeng[] menFengs,
+                                                MenFengServiceRepositorySet menFengServiceRepositorySet) {
         PanRepository panRepository = menFengServiceRepositorySet.getPanRepository();
         Pan pan = panRepository.take(panId);
         List<String> playerIds = pan.allPlayerIds();
         Random random = new Random(panId);
 
-        //随机选一个玩家设为东
-        int dongIndex = random.nextInt(playerIds.size());
-        String dongPlayerId = playerIds.remove(dongIndex);
-        pan.setPlayerMenFeng(dongPlayerId, MenFeng.dong);
+        for (int i = 0; i < menFengs.length; i++) {
+            int index = random.nextInt(playerIds.size());
+            String playerId = playerIds.remove(index);
+            pan.setPlayerMenFeng(playerId, menFengs[i]);
+        }
 
-        //随机选一个玩家设为南
-        int nanIndex = random.nextInt(playerIds.size());
-        String nanPlayerId = playerIds.remove(nanIndex);
-        pan.setPlayerMenFeng(nanPlayerId, MenFeng.nan);
-
-        //随机选一个玩家设为西
-        int xiIndex = random.nextInt(playerIds.size());
-        String xiPlayerId = playerIds.remove(xiIndex);
-        pan.setPlayerMenFeng(xiPlayerId, MenFeng.xi);
-
-        //剩下的玩家设为北
-        String beiPlayerId = playerIds.get(0);
-        pan.setPlayerMenFeng(beiPlayerId, MenFeng.bei);
     }
 }
